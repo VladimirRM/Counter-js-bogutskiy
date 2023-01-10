671ed5aafcc5bed2222ab8bb97a4d8dbe7382e9b
Vladimir RM <shvdrummer@gmail.com>
2023-01-10 09:21:23 +0200

    check all rendering

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import React,{useState} from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [counter, setCounter] = useState(5);
 
-const [counter,setCounter]= useState( 5)
-
-const changePlus = ()=>{
-  setCounter(counter+1)
-}
-const changeMinus = ()=>{
-  setCounter(counter+1)
-}
+  const changePlus = () => {
+    setCounter(counter + 1);
+  };
+  const changeMinus = () => {
+    setCounter(counter - 1);
+  };
 
   return (
     <div className="App">
-       {counter}
-       <button onClick={changePlus}>Plus</button>
-       <button onClick={changeMinus}>Plus</button>
+      {counter}
+      <button onClick={changePlus}>Plus</button>
+      <button onClick={changeMinus}>Minus</button>
     </div>
   );
 }
