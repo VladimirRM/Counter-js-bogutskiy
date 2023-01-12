595f447c766659c1a0aaa1a01bb1b3deabc820ce
Vladimir RM <shvdrummer@gmail.com>
2023-01-12 16:30:32 +0200

    add const plus

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import React,{useState} from 'react'
 const App = () => {
 
   const [count,setCount] = useState(0)
+
+  const plus =()=>{
+    setCount(count + 1)
+  }
   return (
     <div>
       <button>Minus</button>
