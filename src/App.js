33fdb19ab04d06b9796889e88d22826f1f230e34
Vladimir RM <shvdrummer@gmail.com>
2023-01-13 10:43:27 +0200

    create new func incrementPlus

--- src/App.js
+++ src/App.js
@@ -1,34 +1,42 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [counters, setCounters] = useState(0);
-
+  
   const initialCounters = [
     {
-      id:123,
-      value:10,
+      id: 123,
+      value: 10,
     },
     {
-      id:125,
-      value:10,
+      id: 125,
+      value: 10,
     },
     {
-      id:126,
-      value:10,
+      id: 126,
+      value: 10,
     },
-  ]
-
+  ];
+  
+  const [counters, setCounters] = useState(0);
   const incrementPlus = () => {
-    setCounters(counters + 1);
+        const updatedCounts = counters.map(el=>
+          el.id===id ?{...el,value:e.value}:el)
+          setCounters(updatedCounts)
   };
   const decrementMinus = () => {
     setCounters(counters - 1);
   };
   return (
     <div>
-      <button onclick={incrementPlus}>Plus</button>
-      {counters}
-      <button onclick={decrementMinus}>Plus</button>
+      <ul>
+        {counters.map((el) => (
+          <li>
+            <button onclick={()=>incrementPlus()}>Plus</button>
+            {counters}
+            <button onclick={()=>decrementMinus()}>Plus</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
