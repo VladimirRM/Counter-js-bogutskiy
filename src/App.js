053327e7ff4be961bb0b663cfa7022ec79fb1ab5
Vladimir RM <shvdrummer@gmail.com>
2023-01-15 09:38:34 +0200

    update all function

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import React, { useState } from "react";
 const App = () => {
   const [counters, setCounters] = useState(0);
 
-  const increment =()=>{
-    setCounters(counters + 1)
-  }
-  const decrement =()=>{
-    setCounters(counters - 1)
-  }
+  const increment = () => {
+    setCounters(counters + 1);
+  };
+  const decrement = () => {
+    setCounters(counters - 1);
+  };
   return (
     <div>
       <button onClick={() => increment()}>Plus</button>
