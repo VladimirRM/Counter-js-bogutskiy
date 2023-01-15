b74857dc36a82eb57e56df4e2aba5a6e106ec5a8
Vladimir RM <shvdrummer@gmail.com>
2023-01-15 09:36:16 +0200

    add button in decrement

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import React, { useState } from "react";
 
 const App = () => {
   const [counters, setCounters] = useState(0);
-  return <div>App</div>;
+  return <div>
+
+
+      <button onClick={()=>increment()}>Plus</button>
+      {counters}
+      <button onClick={()=>decrement()}>Minus</button>
+
+  </div>;
 };
 
 export default App;
