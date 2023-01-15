d39b4eba19ef8e2824d83becbcb5f39036d88c98
Vladimir RM <shvdrummer@gmail.com>
2023-01-15 09:37:41 +0200

    create func in-decrement

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import React, { useState } from "react";
 
 const App = () => {
   const [counters, setCounters] = useState(0);
-  return <div>
 
-
-      <button onClick={()=>increment()}>Plus</button>
+  const increment =()=>{
+    setCounters(counters + 1)
+  }
+  const decrement =()=>{
+    setCounters(counters - 1)
+  }
+  return (
+    <div>
+      <button onClick={() => increment()}>Plus</button>
       {counters}
-      <button onClick={()=>decrement()}>Minus</button>
-
-  </div>;
+      <button onClick={() => decrement()}>Minus</button>
+    </div>
+  );
 };
 
 export default App;
